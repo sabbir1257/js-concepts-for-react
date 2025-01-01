eb76064b116cc45bb1d63d3761bb1064335e5fce
sabbir1257 <masabbir1257@gmail.com>
2025-01-01 20:42:59 +0600

    object destructuring

--- destruring.js
+++ destruring.js
@@ -7,7 +7,7 @@ const numbers = [42, 65];
 
 const [x, y] = numbers;
 
-function boxify (num1, num2){
+function boxify(num1, num2) {
      const nums = [num1, num2];
      return nums;
 }
@@ -23,6 +23,33 @@ const student = {
      movies: ["king khan", 'Dhakar Mastan']
 };
 
-const [firstMovie , secondMovie] = student.movies;
+const [firstMovie, secondMovie] = student.movies;
 
 
+
+
+// object destructuring
+// const { name, age } = { name: 'alu', age: 14 };
+const {name, age } = { id: 12 ,name: 'alu', salary: 3400, age: 14 };
+
+const employee = {
+     id: 'VS Code',
+     designation: "developer",
+     machine: 'mac',
+     languages: ['html', 'css', 'js'],
+     specification: {
+          height: 66,
+          weight: 67,
+          address: 'kumarkhali',
+          drink: 'water',
+          watch: {
+               color: 'black',
+               price: 500,
+               brand: 'garmin'
+          }
+     }
+}
+
+const {machine, id} = employee;
+const {weight, address} = employee.specification;
+const {brand} = employee?.specification?.watch;
