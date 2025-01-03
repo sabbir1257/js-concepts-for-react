7dc2566fc9300ee920eb604121d717e45221a8ca
sabbir1257 <masabbir1257@gmail.com>
2025-01-03 22:14:17 +0600

    notation file push

--- notation.js
+++ notation.js
@@ -2,7 +2,22 @@ const person = {
      name: 'Lal Nil Holud mia',
      profession: 'Traffic Surgeon',
      age: 39,
+     25: 'Summer',
+     'son-name': 'Sunny',
      address: 'Shahabag'
 }
 
-// dot not
+// dot notation 
+const prof1 = person.profession;
+
+
+// bracket notation
+const prof2 = person["profession"];
+const pName = 'profession';
+const prof3 = person[pName];
+// console.log(prof3)
+
+
+const season = person[25];
+// console.log(season);
+const son = person[son-name];
